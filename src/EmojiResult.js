b32c224fbd0af405aede9f97dee840982404635a
Mehmet VAROL <mehmt.varol@gmail.com>
2019-07-04 15:05:45 +0300

    files added

---
+++ src/EmojiResult.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Clipboard from "clipboard";
+
+import EmojiResultRow from "./EmojiResultRow";
+
+
+export default class EmojiResults extends Component {
+  static propTypes = {
+    emojiData: PropTypes.array
+  };
+
+  componentDidMount() {
+    this.clipboard = new Clipboard(".copy-to-clipboard");
+  }
+
+  componentWillUnmount() {
+    this.clipboard.destroy();
+    console.log("unmount");
+  }
+
+  render() {
+    return (
+      <div className="component-emoji-results">
+        {this.props.emojiData.map(emojiData => (
+          <EmojiResultRow
+            key={emojiData.title}
+            symbol={emojiData.symbol}
+            title={emojiData.title}
+          />
+        ))}
+      </div>
+    );
+  }
+}
---
+++ src/EmojiResultRow.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import "./EmojiResultRow.css"
+
+export default class EmojiResultRow extends Component {
+  static propTypes = {
+    title: PropTypes.string,
+    symbl: PropTypes.string,
+  }
+  render() {
+    const codePointHex = this.props.symbol.codePointAt(0).toString(16);
+    const src = `//cdn.jsdelivr.net/emojione/assets/png/${codePointHex}.png`;
+    return (
+      <div
+        className="component-emoji-result-row copy-to-clipboard"
+        data-clipboard-text={this.props.symbol}
+      >
+        <img src={src} alt={this.props.title} />
+        <span className="title">{this.props.title}</span>
+        <span className="info">Click to copy emoji</span>
+      </div>
+    )
+  }
+}
